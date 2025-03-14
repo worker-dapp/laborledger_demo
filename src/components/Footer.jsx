3245bf0e117ba59591104c1d8b4cd7fc27ae1117
tejaaswini <tejnaren07@gmail.com>
2025-03-13 23:19:17 -0400

    update footer | add ©

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='py-20 text-center'>
-        2025 laborLedger. All rights reserved.
+        © 2025 laborLedger. All rights reserved.
     </div>
   )
 }
