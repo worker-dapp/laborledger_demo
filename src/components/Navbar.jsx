b733dc24cd3eb38cc9c9deed017e1fa2f4dc2bba
tejaaswini <tejnaren07@gmail.com>
2025-03-13 23:18:56 -0400

    update navbar | change color, add style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,34 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className='w-full bg-[#FFD3B1]/20 font-medium'>
-      <div className='flex items-center justify-between px-20 py-8'>
-        <Link to='/' className='text-3xl'>LaborLedger</Link>
-        <div className='flex gap-16 text-lg'>
-            <Link to='/'>Home</Link>
-            <Link to='/about-us'>About Us</Link>
-            <Link>Get Started</Link>
+    <div className='w-full bg-[#FFD3B1]/20 font-medium shadow-md'>
+      <div className='flex items-center justify-between px-20 py-5'>
+        
+        {/* Enhanced Brand Name */}
+        <Link to='/' className='text-4xl font-extrabold tracking-wide'>
+          <span className='text-gray-900 hover:text-orange-600 transition-all'>Labor</span>
+          <span className='text-orange-500'>Ledger</span>
+        </Link>
+
+        {/* Navigation */}
+        <div className='flex items-center gap-12 text-lg'>
+          <NavLink to='/' className={({ isActive }) => 
+            `hover:text-orange-600 transition-all ${isActive ? 'text-orange-600 font-semibold' : 'text-gray-900'}`}>
+            Home
+          </NavLink>
+          <NavLink to='/about-us' className={({ isActive }) => 
+            `hover:text-orange-600 transition-all ${isActive ? 'text-orange-600 font-semibold' : 'text-gray-900'}`}>
+            About Us
+          </NavLink>
+
+          {/* CTA Button - Fixed Alignment */}
+          <Link 
+            to='/get-started' 
+            className='bg-orange-500 text-white px-6 py-2 rounded-lg hover:bg-orange-600 transition-all shadow-md text-lg flex items-center justify-center'>
+            Get Started
+          </Link>
         </div>
       </div>
     </div>
